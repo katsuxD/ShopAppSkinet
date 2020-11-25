d172013d3198abdb96e3090bfcfd3f6220f3f924
katsuxD <khanhb5192@gmail.com>
2020-11-25 23:40:12 +0700

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,6 +34,16 @@ namespace API.Controllers
             return await _repo.GetProductByIdAsync(id);
         }
 
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrand()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
 
+         [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductType()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
     }
 }
