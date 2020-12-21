d02b4cf099b04327a2b12e8cc6db8c0cfed6e276
katsuxD <khanhb5192@gmail.com>
2020-12-21 17:20:45 +0700

    add the home page and header information for all pages

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -4,14 +4,13 @@ import { ShopService } from './shop.service';
 import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
-
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  @ViewChild('search', { static: true }) searchTerm: ElementRef;
+  @ViewChild('search', { static: false }) searchTerm: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
@@ -22,15 +21,12 @@ export class ShopComponent implements OnInit {
     { name: 'Price: Low to High', value: 'priceAsc' },
     { name: 'Price: High to Low', value: 'priceDesc' }
   ];
-
   constructor(private shopService: ShopService) { }
-
   ngOnInit() {
     this.getProducts();
     this.getBrands();
     this.getTypes();
   }
-
   getProducts() {
     this.shopService.getProducts(this.shopParams).subscribe(response => {
       this.products = response.data;
@@ -41,7 +37,6 @@ export class ShopComponent implements OnInit {
       console.log(error);
     });
   }
-
   getBrands() {
     this.shopService.getBrands().subscribe(response => {
       this.brands = [{ id: 0, name: 'All' }, ...response];
@@ -49,7 +44,6 @@ export class ShopComponent implements OnInit {
       console.log(error);
     });
   }
-
   getTypes() {
     this.shopService.getTypes().subscribe(response => {
       this.types = [{ id: 0, name: 'All' }, ...response];
@@ -57,41 +51,34 @@ export class ShopComponent implements OnInit {
       console.log(error);
     });
   }
-
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
     this.shopParams.pageNumber = 1;
     this.getProducts();
   }
-
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
     this.shopParams.pageNumber = 1;
     this.getProducts();
   }
-
   onSortSelected(sort: string) {
     this.shopParams.sort = sort;
     this.getProducts();
   }
-
   onPageChanged(event: any) {
     if (this.shopParams.pageNumber !== event) {
       this.shopParams.pageNumber = event;
       this.getProducts();
     }
   }
-
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
     this.shopParams.pageNumber = 1;
     this.getProducts();
   }
-
   onReset() {
     this.searchTerm.nativeElement.value = '';
     this.shopParams = new ShopParams();
     this.getProducts();
   }
-
 }
